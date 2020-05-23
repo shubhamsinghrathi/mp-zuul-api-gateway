d66b6cd4ae61ae843f275413147e0e6e40ff6f28
Shubham Rathi <shubhamsinghrathi@live.com>
2020-05-23 23:42:16 +0530

    basic setup to enable zuul

---
+++ src/main/java/com/indi/micropro/mpzuulapigateway/AppSecurity.java
@@ -0,0 +1,15 @@
+package com.indi.micropro.mpzuulapigateway;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class AppSecurity extends WebSecurityConfigurerAdapter {
+
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic().disable();
+	}
+	
+}
