a2b084e5cb8efbf486d83d9cbfcd06c5bcf9cccf
Shubham Rathi <shubhamsinghrathi@live.com>
2020-05-23 23:26:29 +0530

    initial commit

---
+++ src/main/java/com/indi/micropro/mpzuulapigateway/MpZuulApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.indi.micropro.mpzuulapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpZuulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpZuulApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/indi/micropro/mpzuulapigateway/MpZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indi.micropro.mpzuulapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpZuulApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
